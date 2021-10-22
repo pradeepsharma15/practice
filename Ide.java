3bc5904ba4cea9349bb2d3d1707fc870afc10e4d
pradeepsharma15 <92980285+pradeepsharma15@users.noreply.github.com>
2021-10-22 14:55:46 +0530

    Add files via upload

---
+++ Ide.java
@@ -0,0 +1,27 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.Scanner;
+import java.lang.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//System.out.println("enetr a number with atleast single digit is zero");
+		int n=1009;
+		double sum=0;
+		int i=0;
+		int r;
+		while(n>0){
+		    r=n%10;
+		    if(r==0){
+		        r=5;
+		    }
+		    
+		    sum=sum+r*Math.pow(10,i) ;
+		    i++;
+		    n=n/10;
+		    
+		}
+		System.out.print(sum);
+	}
+}
